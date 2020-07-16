e8bcfaeb18cb2adec93d745119fbf610688cf788
a1barif2h <a1barif2h@gmail.com>
2020-07-17 03:25:13 +0600

    Added  the BRICKS CALCULATOR

--- assignment.js
+++ assignment.js
@@ -15,3 +15,39 @@ function woodCalculator(chair, table, bed) {
     return totalNeedWood;
 }
 console.log("You nedd ", woodCalculator(28, 32, 42), "Cubic Feet Wood.");
+
+// #Problem-3: BRICKS CALCULATOR
+function brickCalculator(floors) {
+    if (floors <= 0) {
+        return "Please enter an valid floors numbers!"
+    } else if (floors <= 10) {
+        let floorsToFeet = floors * 15;
+        let feetToBricks = floorsToFeet * 1000;
+        return feetToBricks;
+    } else if (floors > 10 && floors <= 20) {
+        let totalFloors = floors;
+        let firstTenFloors = 10;
+        let theRestOfTheFloors = totalFloors - firstTenFloors;
+        let firstTenFloorsToFeet = firstTenFloors * 15;
+        let firstTenFloorsFeetToBricks = firstTenFloorsToFeet * 1000;
+        let theRestOfTheFloorsToFeet = theRestOfTheFloors * 12;
+        let theRestOfTheFloorsFeetToBricks = theRestOfTheFloorsToFeet * 1000;
+        let totalBricksNeeded = firstTenFloorsFeetToBricks + theRestOfTheFloorsFeetToBricks;
+        return totalBricksNeeded;
+    } else if (floors > 20) {
+        let totalFloors = floors;
+        let firstTenFloors = 10;
+        let temporaryTenFloors = totalFloors - firstTenFloors;
+        let theRestOfTheFloors = temporaryTenFloors - 10;
+        let secondTenFloors = temporaryTenFloors - theRestOfTheFloors;
+        let firstTenFloorsToFeet = firstTenFloors * 15;
+        let firstTenFloorsFeetToBricks = firstTenFloorsToFeet * 1000;
+        let secondTenFloorsToFeet = secondTenFloors * 12;
+        let secondTenFloorsFeetToBricks = secondTenFloorsToFeet * 1000;
+        let theRestOfTheFloorsToFeet = theRestOfTheFloors * 10;
+        let theRestOfTheFloorsFeetToBricks = theRestOfTheFloorsToFeet * 1000;
+        let totalBricksNeeded = firstTenFloorsFeetToBricks + secondTenFloorsFeetToBricks + theRestOfTheFloorsFeetToBricks;
+        return totalBricksNeeded;
+    }
+}
+console.log("You need ", brickCalculator(28), " Bricks.");
