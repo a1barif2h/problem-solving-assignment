cfb8937a0289e619bfdee5007b5883d475e10512
a1barif2h <a1barif2h@gmail.com>
2020-07-17 01:33:06 +0600

    Added the wood calculator

--- assignment.js
+++ assignment.js
@@ -1,7 +1,17 @@
-// CONVERTER TO CHANGE FEET TO MILE
+// #Problem-1: CONVERTER TO CHANGE FEET TO MILE
 function feetToMile(feet) {
   let mile = feet * 0.000189394;
   mile = mile.toFixed(8);
   return mile;
 }
 console.log("Mile = ", feetToMile(10));
+
+// #Problem-2: WOOD CALCULATOR
+function woodCalculator(chair, table, bed) {
+    forChair = chair * 1;
+    forTable = table * 3;
+    forBed = bed * 5;
+    let totalNeedWood = forChair + forTable + forBed;
+    return totalNeedWood;
+}
+console.log("You nedd ", woodCalculator(28, 32, 42), "Cubic Feet Wood.");
